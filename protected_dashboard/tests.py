c4b33644cb2110a3e287543908ba08cd92765e35
alex.petukhov <alex@example.com>
2022-11-25 15:41:51 +0400

    Add readme, requirements and tests.

--- protected_dashboard/tests.py
+++ protected_dashboard/tests.py
@@ -1,3 +1,21 @@
 from django.test import TestCase
+from requests import head
 
-# Create your tests here.
+DOMAIN = "http://127.0.0.1:8000/"
+
+PAGES_NO_AUTH = ("users/login/", "users/register/")
+PAGES_NO_AUTH = (DOMAIN + page for page in PAGES_NO_AUTH)
+
+PAGES_WITH_AUTH = ("",)
+PAGES_WITH_AUTH = (DOMAIN + page for page in PAGES_WITH_AUTH)
+
+class PagesTest(TestCase):
+	def test_status_code_200(self):
+		for page in PAGES_NO_AUTH:
+			response = head(page)
+			self.assertEqual(response.status_code, 200)
+
+	def test_status_code_302(self):
+		for page in PAGES_WITH_AUTH:
+			response = head(page)
+			self.assertEqual(response.status_code, 302)
--- users/tests.py
+++ users/tests.py
@@ -1,3 +1,25 @@
 from django.test import TestCase
+from .models import CustomUser
+from .forms import LoginForm, RegisterForm
 
 # Create your tests here.
+class CustomUserTestCase(TestCase):
+
+	@classmethod
+	def setUpTestData(cls):
+		cls.user = CustomUser.objects.create(username="user1", email="user1@example.com", 
+			password="Zaq12wsx")
+		cls.email = cls.user._meta.get_field("email")
+
+	def test_email_unique(self):
+		#Test unique=True for email field
+		real_unique = getattr(self.email, "unique")
+		self.assertTrue(real_unique)
+
+class LoginFormTests(TestCase):
+	def test_username_field_label(self):
+		#Test a label of username field
+		form = LoginForm()
+		username_label = form.fields["username"].label
+
+		self.assertEqual(username_label, "Email Address")
